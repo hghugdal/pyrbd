4802a5fd0b0d8b09a1cdbbf4a52e374bc6237667
H. G. Hugdal <hghugdal+github@gmail.com>
2025-09-11 22:10:20 +0200

    Improve group arrow handling (#9)
    The way arrows are drawn inside Groups is improved to handle blocks or Series with length.
    
    Fixes #3

--- docs/examples/layered_rbd.py
+++ docs/examples/layered_rbd.py
@@ -9,28 +9,41 @@ chdir(path.dirname(__file__))
 
 # Define the blocks comprising the diagram
 start_block = Block("Start", "blue!30")
-block = Block("Basic block", "gray")
+block = Block("Block", "gray")
 group_1 = Group(
     [deepcopy(b) + deepcopy(b) for b in 3 * [block]],
-    text="Group",
+    text="Group with Series",
     color="orange",
     parent=start_block,
 )
 series_1 = Series(
-    [deepcopy(block), 2 * deepcopy(block)], text="Series", color="red", parent=group_1
+    [deepcopy(block), 2 * deepcopy(block)],
+    text="Series with Group",
+    color="red",
+    parent=group_1,
 )
 series_2 = Series(
     [2 * deepcopy(block), deepcopy(block), 3 * (deepcopy(block) + deepcopy(block))],
-    text="Series",
+    text="Series with mixed Groups",
     color="RoyalBlue",
     parent=series_1,
 )
-end_block = Block("End", "green!50", parent=series_2)
+group_2 = Group(
+    [
+        deepcopy(block) + deepcopy(block) + deepcopy(block),
+        deepcopy(block),
+        deepcopy(block) + deepcopy(block) + deepcopy(block) + deepcopy(block),
+    ],
+    text="Group with mixed Series",
+    color="red",
+    parent=series_2,
+)
+end_block = Block("End", "green!50", parent=group_2)
 
 # Define and compile the diagram
 diagram = Diagram(
     "layered_RBD",
-    blocks=[start_block, group_1, series_1, series_2, end_block],
+    blocks=[start_block, group_1, series_1, series_2, group_2, end_block],
 )
 diagram.write()
 diagram.compile(["pdf", "svg"])
