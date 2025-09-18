ef0fd7df657a305ccce75e8ec5580f21f4ef2418
H. G. Hugdal <hghugdal+github@gmail.com>
2025-09-18 21:50:24 +0200

    Change install method to not editable

--- pyrbd/__init__.py
+++ pyrbd/__init__.py
@@ -1,7 +1,11 @@
 """Package for creating simple reliability block diagrams using LaTeX TikZ."""
 
+from importlib.metadata import version
+
 from .block import Block, Series, Group
 from .diagram import Diagram
+
+__version__ = version("pyrbd")
 
 __all__ = [
     "Block",
